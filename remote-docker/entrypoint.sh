c2f38c56885624110899b49f1cbb0043b70dffe1
Stephen M. Coakley <me@stephencoakley.com>
2019-02-05 11:35:48 -0600

    Make host verification more secure

--- remote-docker/entrypoint.sh
+++ remote-docker/entrypoint.sh
@@ -6,23 +6,33 @@ if [ -z "$DOCKER_REMOTE_HOST" ]; then
     exit 1
 fi
 
-# If a private SSH key is provided for an SSH-based connection, register it with the SSH agent.
-if [ -n "${DOCKER_SSH_KEY:-}" ]; then
-    echo "Registering SSH private key..."
+# Extra handling for SSH-based connections.
+if [ ${DOCKER_REMOTE_HOST#"ssh://"} != "$DOCKER_REMOTE_HOST" ]; then
+    SSH_HOST=${DOCKER_REMOTE_HOST#"ssh://"}
+    SSH_HOST=${SSH_HOST#*@}
 
+    if [ -z "$DOCKER_SSH_PRIVATE_KEY" ]; then
+        echo "DOCKER_SSH_PRIVATE_KEY is required for SSH hosts!"
+        exit 1
+    fi
+
+    if [ -z "$DOCKER_SSH_PUBLIC_KEY" ]; then
+        echo "DOCKER_SSH_PUBLIC_KEY is required for SSH hosts!"
+        exit 1
+    fi
+
+    echo "Registering SSH keys..."
+
+    # Save private key to a file and register it with the agent.
     mkdir -p "$HOME/.ssh"
-
-    printf 'Host *\n\tStrictHostKeyChecking no\n' >> "$HOME/.ssh/config"
-    chmod 400 "$HOME/.ssh/config"
-
-    printf '%s' "$DOCKER_SSH_KEY" > "$HOME/.ssh/docker"
+    printf '%s' "$DOCKER_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
     chmod 600 "$HOME/.ssh/docker"
-
     eval $(ssh-agent)
     ssh-add "$HOME/.ssh/docker"
-    sleep 10
+
+    # Add public key to known hosts.
+    printf '%s %s\n' "$SSH_HOST" "$DOCKER_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
 fi
 
 echo "Connecting to $DOCKER_REMOTE_HOST..."
-
 docker --log-level debug --host "$DOCKER_REMOTE_HOST" "$@" 2>&1
